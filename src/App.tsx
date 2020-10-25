6b4c3ac8fa1f706b20502858733935cc3d925854
mikeda <baguzy@gmail.com>
2020-10-25 11:28:30 +0900

    初期実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useFormik } from 'formik'
 
-function App() {
+function SignupForm() {
+  const formik = useFormik({
+    initialValues: {
+      email: ''
+    },
+    onSubmit: values => {
+      alert(JSON.stringify(values, null, 2));
+    }
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={formik.handleSubmit}>
+      <label htmlFor="email">メールアドレス</label>
+      <input
+        id="email"
+        name="email"
+        type="email"
+        onChange={formik.handleChange}
+        value={formik.values.email}
+      />
+      <button type="submit">Submit</button>
+    </form>
   );
 }
 
-export default App;
+export default SignupForm;
