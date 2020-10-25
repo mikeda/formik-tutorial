40d4a1667a06561318e01de991d055250dba03bf
mikeda <baguzy@gmail.com>
2020-10-25 11:39:28 +0900

    入力項目追加

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { useFormik } from 'formik'
 function SignupForm() {
   const formik = useFormik({
     initialValues: {
+      firstName: '',
+      lastName: '',
       email: ''
     },
     onSubmit: values => {
@@ -13,6 +15,24 @@ function SignupForm() {
 
   return (
     <form onSubmit={formik.handleSubmit}>
+      <label htmlFor="firstName">First Name</label>
+      <input
+        id="firstName"
+        name="firstName"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.firstName}
+      />
+
+      <label htmlFor="lastName">Last Name</label>
+      <input
+        id="lastName"
+        name="lastName"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.lastName}
+      />
+
       <label htmlFor="email">メールアドレス</label>
       <input
         id="email"
